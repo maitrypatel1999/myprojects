9cf4fac622c4e36f1550fcc23b6feb0845959d49
maitrypatel1999 <maitrypatel1999@gmail.com>
2023-07-03 17:56:16 -0400

    added a lombok depedencies

--- src/main/java/com/example/myprojects/controller/HelloWorldController.java
+++ src/main/java/com/example/myprojects/controller/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.example.myprojects.controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
+@Slf4j
 public class HelloWorldController {
 
     @GetMapping("/")
     public String get() {
+//        log.info("calling get api");
         return "Get";
     }
     @PostMapping("/")
